647a8c1fc38c59ea3aa3ba31dc392448e9936176
Shashank Biplav <Biplavshashank7@gmail.com>
2020-10-05 21:07:16 +0530

    Added recipe model

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/foundation.dart';
 
 class Category {
-final String title;
-final String colorA;
-final String colorB;
-final String iconImageUrl;
-final List<String> recipes;
+  final String title;
+  final String colorA;
+  final String colorB;
+  final String iconImageUrl;
+  final List<String> recipes;
 
-Category({
-@required this.title,
-@required this.colorA,
-@required this.colorB,
-@required this.iconImageUrl,
-@required this.recipes
-});
+  Category(
+      {@required this.title,
+      @required this.colorA,
+      @required this.colorB,
+      @required this.iconImageUrl,
+      @required this.recipes});
 }
---
+++ lib/models/recipe.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+
+class Recipe {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final List<String> ingredients;
+  final List<String> categories;
+  final List<String> steps;
+  final String chef;
+  final String complexity;
+  final String affordability;
+  final String isVegetarian;
+
+  Recipe(
+      {@required this.title,
+      @required this.imageUrl,
+      @required this.duration,
+      @required this.ingredients,
+      @required this.categories,
+      @required this.steps,
+      @required this.chef,
+      @required this.complexity,
+      @required this.affordability,
+      @required this.isVegetarian});
+}
