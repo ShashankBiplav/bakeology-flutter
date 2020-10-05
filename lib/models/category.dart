a87aeedeba21c42d5e5edd4752ec302e61e1a735
Shashank Biplav <Biplavshashank7@gmail.com>
2020-10-05 20:58:23 +0530

    Added category model

---
+++ lib/models/category.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class Category {
+final String title;
+final String colorA;
+final String colorB;
+final String iconImageUrl;
+final List<String> recipes;
+
+Category({
+@required this.title,
+@required this.colorA,
+@required this.colorB,
+@required this.iconImageUrl,
+@required this.recipes
+});
+}
